60a157414bfcee97e0ada73b62dccc89d3a84784
Stanislav Bezruchenko <christopherhell23@gmail.com>
2021-09-02 04:14:50 +0300

    first commit

---
+++ src/main/java/org/example/dao/UserDAOImpl.java
@@ -0,0 +1,66 @@
+package org.example.dao;
+
+import org.example.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.util.List;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public List<User> getAllUser() {
+        return entityManager.createQuery("SELECT u from User u",User.class).getResultList();
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return entityManager.createQuery("SELECT u from User u LEFT join fetch u.roles where u.id=:id",User.class)
+                .setParameter("id",id).getSingleResult();
+    }
+
+    @Override
+    @Transactional
+    public void save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        entityManager.merge(user);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        entityManager.createQuery("DELETE FROM User u WHERE u.id=:id")
+                .setParameter("id", id)
+                .executeUpdate();
+    }
+
+    @Override
+    public User findByLogin(String login) {
+        return entityManager.createQuery("SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.login=:login ", User.class)
+                .setParameter("login", login)
+                .getSingleResult();
+    }
+
+    @Override
+    @Transactional
+    public void update(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        entityManager.merge(user);
+    }
+}
